56aa9e908e88c27359b700189795742e631dc163
Jacob Bubble <Jacob.bubble1@gmail.com>
2015-07-13 03:53:08 -0700

    Checking all addresses in Address block instead of the last one.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,12 @@ var derived;
 var minerFee = 0.0005;
 var utos = []; // Everything spendable in HD Seed
 var totalBalance = 0, tbInSatoshis = 0;
-var blockSize = 50; // Chunk of addresses to check for at a time.
+var blockSize = 50; // Chunk of addresses to check for at a time. Not to be confused with Bitcoin Blocks
+
+// Per address
+var unconfirmed = 0;
+var totalReceived;
+var used = true; // By default, assume addrs are used.
 
 var hdPrivateKey;
 var bitcoinB = '\u0E3F'; var mBitcoin = 'm'+'\u0E3F';
@@ -21,10 +26,12 @@ var bitcoinB = '\u0E3F'; var mBitcoin = 'm'+'\u0E3F';
 var empty = "Invalid entropy: must be an hexa string or binary buffer, got ", emptyResponse = "No Seed";
 var invalidSeed = "Invalid entropy: at least 128 bits needed, got \"�\"", invalidResponse = "Invalid Seed";
 
+// ** Process HD Seed ** 
+
 function processSeed(prs){
 	$(".error-screen").addClass( "hidden"); // Hide
 	$(".loading-screen").toggleClass( "hidden"); // Show
-	console.log("Start");
+	console.log("Start Processing Private Key");
 	
 	index = 0;
 	addressBlock = [];
@@ -34,27 +41,26 @@ function processSeed(prs){
 	
 	hdPrivateKey = new HDPrivateKey.fromSeed(prs);
 	
-	getAddresses();
+	setAddresses();
 }
-
-function getAddresses(){
+function setAddresses(){
 	for(var x = 0;x <= blockSize ;x++){
 		// Derive the next address.
 		derived = hdPrivateKey.derive("m/0/0/" + index.toString());
 		address = derived.privateKey.toAddress();
 		privKeySet.push(derived.privateKey.toWIF());
-		addressBlock[x] = address.toString();
+		addressBlock.push(address.toString());
 		
 		index++;
 	}
-	getUTXOs(addressBlock);
+	setUTXOs(addressBlock);
 }
 
-function getUTXOs(arrayOfAddresses){
-	arrayOfAddresses = arrayOfAddresses.join(); // Comma seperated addys.
+function setUTXOs(arrayOfAddresses){
+	arrayOfAddresses = arrayOfAddresses.join(); // Comma seperated addrs.
 	$.get(api + "addrs/" + arrayOfAddresses + "/utxo", function( data ) {
 		extractUTOs(data);
-		checkAddresses();
+		checkAddrBlock();
 	});
 }
 function extractUTOs(data){
@@ -63,56 +69,47 @@ function extractUTOs(data){
 	}
 }
 
-function checkAddresses(){
-	// Check that the addy has had money sent to it. i.e. been used.
-	$.ajax({
-		 async: true,
-		 type: 'GET',
-		 url: api + "addr/" + addressBlock[addressBlock.length-1] + "/totalReceived",
-		 success: function(data) {
-			 var totalReceived = data;
-			 var unconfirmed = 0;
-				$.get( api + "addr/" + addressBlock[addressBlock.length-1] + "/unconfirmedBalance", function( data ) {
-					if(sweepUnconfirmed){
-						if(data >= 0) { unconfirmed = data };
-					}
-					if((totalReceived + unconfirmed) > 0){
-						getAddresses();
-				 } else {
-					 getBalance(utos);
-					 var totalToSend = totalBalance - minerFee;
-					 $(".balance").text("Total To Send: " + bitcoinB + " " + totalToSend );
-					 $(".loading-screen").toggleClass("hidden"); // Hide
-				 }
-				});
+function checkAddrBlock(){
+	// Check that at least one addr in addressBlock has had money sent to it. i.e. been used.
+	for(var counter = (addressBlock.length-blockSize)/*50 - 50 = 0;*/; counter <= blockSize; counter++){
+		console.log("Checking addr #" + counter + " : " + addressBlock[counter]);
+		checkAddr(addressBlock[counter]);
+	}
+	if(used){
+		setAddresses();
+	} else{ // If no used in block, then assume there's no more used addrs in the Seed, finish proccess.
+		finishProcessingSeed();
+	}
+}
+function checkAddr(addr){
+	$.get( api + "addr/" + addr + "/totalReceived", function(data){
+		var totalReceived = data;
+		unconfirmed = 0;
+		if(sweepUnconfirmed){
+			setUnconfirmed(addr);
+		} else {
+			checkIfUsed();
 		}
+	 });
+}
+function setUnconfirmed(addr){
+	$.get( api + "addr/" + addr + "/unconfirmedBalance", function( data ) {
+		if(data >= 0) { unconfirmed = data };
+		checkIfUsed();
 	});
+}
+function checkIfUsed(){
+	if((totalReceived + unconfirmed) > 0){
+		used = true;
+	} else {
+		used = false;
+	}
 }
 function getBalance(arrayOfUtos){
 	for(x in arrayOfUtos){
 			totalBalance += arrayOfUtos[x].amount;
 	}
 	return totalBalance;
-}
-function sweepFunds(toBTCAddy){
-	console.log("Start Sweep");
-	var transaction = createTransaction(toBTCAddy);
-	console.log(transaction.serialize() );
-  
-  $.post(  api + "tx/send", transaction.serialize())
-  .done(function( data ) {
-    alert( "Transaction Sent: " + data );
-  });
-}
-function createTransaction(addy){
-    console.log("Miner Fee: " + btcToSatoshis(minerFee))
-	var transaction = new bitcore.Transaction()
-    .from(utos)          
-    .to(addy, btcToSatoshis(totalBalance))
-    .change(addy) // Send everything, even change for sweep
-    .fee(btcToSatoshis(minerFee))
-    .sign(privKeySet);
-    return transaction;
 }
 
 function transErr(e){
@@ -130,6 +127,36 @@ function transErr(e){
 			response = e;
 	}
 	return response;
+}
+
+function finishProcessingSeed(){
+	$(".loading-screen").toggleClass("hidden"); // Hide
+	getBalance(utos);
+	var totalToSend = (totalBalance - minerFee);
+	$(".balance").text("Total To Send: " + bitcoinB + " " + totalToSend );
+}
+
+// ** SWEEP FUNDS ** 
+
+function sweepFunds(toBTCAddr){
+	console.log("Start Sweep");
+	var transaction = createTransaction(toBTCAddr);
+	console.log(transaction.serialize() );
+  
+  $.post(  api + "tx/send", transaction.serialize())
+  .done(function( data ) {
+    alert( "Transaction Sent: " + data );
+  });
+}
+function createTransaction(addr){
+    console.log("Miner Fee: " + btcToSatoshis(minerFee))
+	var transaction = new bitcore.Transaction()
+    .from(utos)          
+    .to(addr, btcToSatoshis(totalBalance))
+    .change(addr) // Send everything, even change for sweep
+    .fee(btcToSatoshis(minerFee))
+    .sign(privKeySet);
+    return transaction;
 }
 
 function btcToSatoshis(btcAmt){
@@ -151,7 +178,7 @@ $(function() {
 	});
 	
 	$("#sweep").click(function() {
-		var userAddy = $("#btcAddy").val();
-		sweepFunds(userAddy);
+		var useraddr = $("#btcAddr").val();
+		sweepFunds(useraddr);
 	});
 });
