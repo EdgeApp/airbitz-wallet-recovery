5d42c8aef02d05e9fd257d8326a5bbfb5906bd6b
Jacob Bubble <Jacob.bubble1@gmail.com>
2015-08-12 15:43:42 -0700

    Adding gulpfiles to debug.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var inline = require('gulp-inline')
+    , uglify = require('gulp-uglify')
+    , minifyCss = require('gulp-minify-css');
+
+gulp.task('default', function() {
+  // place code for your default task here
+   
+  gulp.src('public/index.html')
+    .pipe(inline({
+      base: 'public/',
+      js: uglify(),
+      css: minifyCss(),
+      disabledTypes: ['svg', 'img', 'js'], // Only inline css files 
+      ignore: ['./css/do-not-inline-me.css']
+    }))
+    .pipe(gulp.dest('dist/'));
+});
